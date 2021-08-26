3f351ba8ab9da5c527096b72b9cb70b6c65f5143
Luis Henrique Ries <prof.ries@gmail.com>
2021-08-26 20:15:14 -0300

    Exercicio 2

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/produtos', (req, res) => {
+  res.send('Listando de Produtos')
 })
 
-app.get('/hello', (req, res) => {
-    res.send('Hello Everybody!')
-  })
-  
+app.get('/produtos/:id', (req, res) => {
+  res.send('Buscando pelo id '+req.params.id);
+})
+
+app.post('/produtos', (req, res) => {
+  res.status(201).send('Adicionando produto');
+})
+
+app.put('/produtos/:id', (req, res) => {
+  res.send('Alterando o produto '+req.params.id);
+})
+
+app.delete('/produtos/:id', (req, res) => {
+  res.send('Deletando o produto '+req.params.id)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-console.log("Servidor ja chamou listen");
